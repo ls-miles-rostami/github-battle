139425d7f91e4c0867723b2fb8fca00dc0b5659c
milesros <miles.rostami@gmail.com>
2018-05-02 15:08:20 -0400

    env variables

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,6 +1,5 @@
-const keys = {
-  github_clientId: '31b04a90f434c2d06ea0',
-  github_clientSecret: 'a524224c3c26b5cede584f27ed274c0e82011d1f'
-}
-
-export default keys
+if(process.env.NODE_ENV === 'production'){
+  module.exports = require('./keys_prod')
+}else{
+  module.exports = require('./keys_dev')
+} 
---
+++ src/config/keys_prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  github_clientId: process.env.GITHUB_CLIENTID,
+  github_clientSecret: process.env.GITHUB_CLIENTSECRET
+}
